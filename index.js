1fc16efe43b73258dc73d0744005692809811f4c
chupacambra <jovankoul@hotmail.com>
2023-09-22 19:58:23 +0300

    Buffers, Streams, Server,

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const buffer = new Buffer.from('Gkov');
-console.log(buffer.toJSON());
-console.log(buffer);
+///
+///
+///
+///
+const fs = require('node:fs');
+const readableStream = fs.createReadStream('./file.txt', {
+  encoding: 'utf-8',
+  highWaterMark: 2,
+});
+
+const writableStream = fs.createWriteStream('./file2.txt', {
+  encoding: 'utf-8',
+});
+
+readableStream.on('data', (chunk) => {
+  console.log(chunk);
+  writableStream.write(chunk);
+});
+console.log('hi');
