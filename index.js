4dfd4aa31d9e9d2d5c8f80008261c11c93d6f426
chupacambra <jovankoul@hotmail.com>
2023-09-26 14:08:24 +0300

    Event Loop

--- index.js
+++ index.js
@@ -2,25 +2,16 @@
 ///
 ///
 ///
-const http = require('node:http');
-const fs = require('node:fs');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Home Page');
-  } else if (req.url === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('About Page');
-  } else if (req.url === '/api') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ firstName: 'Bruce', lastName: 'Wayne' }));
-  } else {
-    res.writeHead(404);
-    res.end('Page not found');
-  }
-});
+setTimeout(() => console.log('hi from timeout 1'), 0);
+setTimeout(() => console.log('hi from timeout 2 '), 0);
+Promise.resolve().then(() => console.log('hi from promise'));
 
-server.listen(3000, () => {
-  console.log('server running on port 3000');
+process.nextTick(() => console.log('hi from next.tick1'));
+process.nextTick(() => {
+  console.log("'hi from next.tick2'");
+  process.nextTick(() => {
+    console.log('hi from inner next tick');
+  });
 });
+process.nextTick(() => console.log('hi from next.tick4'));
