85c2b28a00d99e34c03f284c3ceca6ba3485411a
chupacambra <jovankoul@hotmail.com>
2023-09-27 14:39:54 +0300

    CLI

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 ///
 ///
 ///
-const fs = require('node:fs');
+const getFiveMoves = async (pokename) => {
+  const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokename}`);
+  const response = await request.json();
+  const moves = response.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+
+getFiveMoves('charmander');
